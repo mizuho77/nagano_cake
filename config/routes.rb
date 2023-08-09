66ce66b9d81417338c2e2be1e848254df449d33f
mizuho77 <132583580+mizuho77@users.noreply.github.com>
2023-08-09 11:17:29 +0000

    マイグレーションファイル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+#  devise_for :admins
+#  devise_for :customers
+  get 'items/index'
+  get 'items/show'
+  get 'homes/top'
+  get 'homes/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
