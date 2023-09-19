6f22bdcaaca889a4accc617f4ffd09b54903e62d
mizuho77 <132583580+mizuho77@users.noreply.github.com>
2023-09-19 11:30:22 +0000

    退会機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
 
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+  namespace :admin do
+    get "/" => "homes#top"
+    resources :items, only: [:index, :new, :show, :edit]
+    resources :customers, only: [:index, :show, :edit]
+  end
+
+
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
@@ -10,21 +23,8 @@ devise_for :customers,skip: [:passwords], controllers: {
   scope module: :public do
     root to: "homes#top"
     get "about" => "homes#about"
-  end
-
-  namespace :public do
     resources :items, only: [:index, :show]
     resources :customers, only: [:show, :edit, :check]
-  end
-
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-
-  namespace :admin do
-    root to: "homes#top"
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
