045b724a24abff410cfb74af26935ed3e44f2838
mizuho77 <132583580+mizuho77@users.noreply.github.com>
2023-11-02 14:20:32 +0000

    20231102商品一覧画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,21 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.page(params[:page]).per(10)
   end
 
   def new
+    @item = Item.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image)
   end
 end
